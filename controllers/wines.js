e8d6208eda123c034a98bf3a347a508c1d2df485
manfredjoa <joamanfred@gmail.com>
2023-08-10 14:39:13 -0400

    fix error messages

--- controllers/wines.js
+++ controllers/wines.js
@@ -3,7 +3,7 @@ import WineData from "../models/WineData.js";
 // Gets all wines
 export const getWines = async (req, res) => {
   try {
-    let wines = await WineData.find();
+    const wines = await WineData.find();
     res.json(wines);
   } catch (error) {
     console.log(error);
@@ -16,7 +16,7 @@ export const getWineById = async (req, res) => {
   try {
     const wine = await WineData.findById(id);
     if (!wine) {
-      return res.status(404).json({ message: "Invalid ID" });
+      return res.status(404).json({ message: "Invalid Wine ID" });
     }
     res.json(wine);
   } catch (error) {
@@ -30,7 +30,7 @@ export const getWineByWhite = async (req, res) => {
   try {
     const wine = await WineData.find({ ProductType: "White Wine" });
     if (!wine) {
-      return res.status(404).json({ message: "Invalid ID" });
+      return res.status(404).json({ message: "No white wines found." });
     }
     res.json(wine);
   } catch (error) {
@@ -44,7 +44,7 @@ export const getWineByRed = async (req, res) => {
   try {
     const wine = await WineData.find({ ProductType: "Red Wine" });
     if (!wine) {
-      return res.status(404).json({ message: "Invalid ID" });
+      return res.status(404).json({ message: "No red wines found." });
     }
     res.json(wine);
   } catch (error) {
@@ -56,9 +56,9 @@ export const getWineByRed = async (req, res) => {
 // Gets all rosé wines
 export const getWineByRose = async (req, res) => {
   try {
-    let wine = await WineData.find({ ProductType: "Rosé Wine" });
+    const wine = await WineData.find({ ProductType: "Rosé Wine" });
     if (!wine) {
-      return res.status(404).json({ message: "Invalid ID" });
+      return res.status(404).json({ message: "No rosé wines found." });
     }
     res.json(wine);
   } catch (error) {
@@ -70,7 +70,9 @@ export const getWineByRose = async (req, res) => {
 // Gets all sparkling wines, note VARIETAL TYPE vs. PRODUCT TYPE
 export const getWineBySparkling = async (req, res) => {
   try {
-    const wine = await WineData.find({ VarietalType: "Sparkling" });
+    const wine = await WineData.find({
+      VarietalType: "No sparkling wines found.",
+    });
     if (!wine) {
       return res.status(404).json({ message: "Invalid ID" });
     }
@@ -87,7 +89,9 @@ export const getWineByCountryState = async (req, res) => {
     const { country } = req.params;
     const wine = await WineData.find({ CountryState: country });
     if (!wine) {
-      return res.status(404).json({ message: "Invalid ID" });
+      return res
+        .status(404)
+        .json({ message: "No wines from that country found." });
     }
     res.json(wine);
   } catch (error) {
@@ -95,17 +99,3 @@ export const getWineByCountryState = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
-
-// export const getWineBySuggested = async (req, res) => {
-//   try {
-//     const { Suggested } = req.params;
-//     const wine = await WineData.find({ Suggested });
-//     if (!wine) {
-//       return res.status(404).json({ message: "Invalid ID" });
-//     }
-//     res.json(wine);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
